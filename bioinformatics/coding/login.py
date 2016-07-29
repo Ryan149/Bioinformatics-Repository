0b244756054a2cad90170650dafb9dc7abd96711
Ryan149 <ryanthorn@me.com>
2016-07-29 19:47:32 +0000

    login.py

--- bioinformatics/coding/login.py
+++ bioinformatics/coding/login.py
@@ -1,1 +1,21 @@
-login=
+condition1="false"
+condition2="false"
+truelogin="U53rNam3"
+truepassword="pa55W0rd"
+while condition1=="false":
+ login = raw_input('Enter your username:')
+ if (login==truelogin):
+  break
+  condition1="true"
+ if (login!=truelogin):
+  print "You have entered the incorrect username"
+  condition1="false"
+while condition2=="false":
+  password = raw_input('Enter your password:')
+  if (password==truepassword):
+   break
+   condition1="true"
+  if (password!=truepassword):
+   print "You have entered the incorrect password"
+   condition1="false"
+print "Good job! You logged in!"
